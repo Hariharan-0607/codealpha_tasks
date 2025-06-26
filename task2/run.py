9115370169fc6c1bce2d9a4adaabf34db7ca6df8
Hariharan-0607 <haripreeth06@gmail.com>
2025-06-26 12:45:07 +0530

    Add files via upload

---
+++ task2/run.py
@@ -0,0 +1,11 @@
+n=1
+while n!='bye':
+    n=input("Enter command/text")
+    if n.lower()=='hello':
+        print("Hi")
+    elif n.lower()=='how are you':
+        print("I'm fine")
+    elif n.lower()=='bye':
+        print('thanks!')
+        print("Goodbye")
+
